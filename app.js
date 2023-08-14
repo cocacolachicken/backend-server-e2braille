3a3f8c2d21210a31e870f9d24287a7ca73ffdbab
tyler <tyler.gu@ashbury.ca>
2023-08-13 20:25:20 -0400

    commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const PDFDoc = require('pdfkit');
+const BlobStream = require('blob-stream');
+
 
 const app = express();
 const port = 3000;
@@ -31,7 +34,38 @@ app.get('/main', (req, res) => {
 app.post('/translate', bodyParser.json(), (req, res) => {
     console.log(req.body);
     res.json("⠠⠔⠀⠠⠏⠁⠎⠁⠙⠢⠁⠀⠎⠬⠔⠄⠂⠀⠦⠠⠙⠂⠀⠙⠤⠙⠤⠙⠂⠀⠙⠴");
-})
+});
+
+app.post('/getPDF', bodyParser.json(), async (req, res) => {
+    console.log(req.body);
+
+    const doc = new PDFDoc();
+    const stream = doc.pipe(BlobStream());
+
+    doc.font('Symbola.ttf').fontSize(24);
+
+    let arr = req.body.translation.split('⠀');
+    for (let word of arr) {
+        doc.text(`${word}⠀`, {
+            lineGap: 8,
+            continued : true,
+            letterSpacing: -5
+        })
+    }
+
+    let blob;
+
+    doc.end();
+    await stream.on('finish', () => {
+        blob = stream.toBlob('application/pdf');
+        console.log(blob);
+        res.type(blob.type);
+        blob.arrayBuffer().then((buf) => {
+            res.send(Buffer.from(buf))
+            }
+        )
+    });
+});
 
 app.listen(port, () => {
     console.log('heyy');
--- public/scripts/mainpage.js
+++ public/scripts/mainpage.js
@@ -2,20 +2,46 @@ let res = "";
 
 
 window.onload = () => {
-    document.getElementById('submit').addEventListener('click', async () => {
+    console.log('hi');
+
+    document.getElementById('translate-submit').addEventListener('click', async () => {
         const request = await fetch ("/translate", {
             method : 'POST',
             headers : {
                 'Content-Type' : 'application/json'
             },
-            body : JSON.stringify({translate : document.getElementById('translate-text').value})
+            body : JSON.stringify({translate : document.getElementById('tl-left').value})
         });
 
         res = await request.json();
 
         console.log(res);
 
-        document.getElementById('result').value = res;
+        document.getElementById('tl-right').value = res;
 
+    });
+
+    document.getElementById('clear-button').addEventListener('click', () => {
+        document.getElementById('tl-left').value = '';
+    });
+
+    document.getElementById('copy-text').addEventListener('click', () => {
+        navigator.clipboard.writeText(res);
+        window.alert('Text copied');
+    });
+
+    document.getElementById('download-text').addEventListener('click', async () => {
+        const request = await fetch ("/getPDF", {
+            method : 'POST',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({translation : document.getElementById('tl-right').value})
+        });
+
+        let pdf = await request.blob();
+
+        let pdfURL = URL.createObjectURL(pdf);
+        window.open(pdfURL);
     });
 };
